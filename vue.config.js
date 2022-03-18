50b34769800b11f2a5aa439d5a62c9daf96d24d9
Rasmus Svanberg <svra18cy@student.ju.se>
2022-03-18 08:21:31 +0100

    Test

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,5 @@
 module.exports = {
-  publicPath: process.env.NODE_ENV === 'production'
-    ? '/zyntaax.github.io'
-    : '/',
+  publicPath: '/', // process.env.NODE_ENV === 'production'
+  // ? '/zyntaax.github.io'
+  // : '/',
 };
