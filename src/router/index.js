56374b2643a156f67cf2eeeb8bf7c129b7c5443b
Rasmus Svanberg <svra18cy@student.ju.se>
2022-03-18 09:05:30 +0100

    Preparing for GH Pages deploy

--- src/router/index.js
+++ src/router/index.js
@@ -5,7 +5,7 @@ import HomeView from '@/views/home/home.view.vue';
 import AboutView from '@/views/about/about.view.vue';
 import ContactView from '@/views/contact/contact.view.vue';
 import ProjectsView from '@/views/projects/projects.view.vue';
-import NotFoundView from '@/views/404/404.vue';
+// import NotFoundView from '@/views/404/404.vue';
 
 // eslint-disable-next-line max-len
 // const BASE_URL = '/portfolio-vuejs'; // process.env.NODE_ENV === 'production' ? '/portfolio-vuejs' : '';
@@ -38,11 +38,11 @@ const routes = [
   },
 
   /* This catchAll should always be placed as the last route-check! */
-  {
-    path: '/:catchAll(.*)',
-    name: '404',
-    component: NotFoundView,
-  },
+  // {
+  //   path: '/:catchAll(.*)',
+  //   name: '404',
+  //   component: NotFoundView,
+  // },
 ];
 
 const router = createRouter({
