62ebe99a81e972c6368739e3253e8e885284cc92
Rasmus Svanberg <svra18cy@student.ju.se>
2022-03-18 10:29:57 +0100

    Preparing for GH Pages deploy

--- vue.config.js
+++ vue.config.js
@@ -1,9 +1,9 @@
 module.exports = {
-  // publicPath: '/', // process.env.NODE_ENV === 'production'
+  publicPath: '/', // process.env.NODE_ENV === 'production'
   // ? '/zyntaax.github.io'
   // : '/',
   // do not use "./dist/"
-  publicPath: '/dist/',
+  // publicPath: '/dist/',
   // make the index.html file place at the root of the repo
-  indexPath: '../index.html',
+  // indexPath: '../index.html',
 };
