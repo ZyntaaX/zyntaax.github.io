c32708841ecac507e880d02e67b29db376d5efde
Rasmus Svanberg <svra18cy@student.ju.se>
2022-03-18 21:51:37 +0100

    Preparing Deploy

--- scripts/gh-pages-deploy.js
+++ scripts/gh-pages-deploy.js
@@ -16,6 +16,15 @@ const fs = require('fs');
     await execa('rm', ['-r', folderName]);
     await execa('git', ['checkout', '-f', 'master']);
     await execa('git', ['branch', '-D', 'gh-pages']);
+    /* * * WIP * * * */
+    await execa('find', ['./', '-type', 'f',
+      '-name', '"index.html"', '|', 'while', 'read', 'fname;', 'do',
+      'dirname=`dirname "$fname"`', 'foldername=`basename "$dirname"`',
+      'filename=`basename $fname`', 'newname=`404.html`',
+      // eslint-disable-next-line no-template-curly-in-string
+      'cp "${dirname}/$filename" "./404.html"', 'done',
+    ]);
+    /* * * * * * * * */
     console.log('Successfully deployed');
   } catch (e) {
     console.log(e.message);
