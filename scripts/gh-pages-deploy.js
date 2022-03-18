0ffd82f0d7908025feb6336397505c1a0327effe
Rasmus Svanberg <svra18cy@student.ju.se>
2022-03-18 22:09:34 +0100

    Preparing Deploy

--- scripts/gh-pages-deploy.js
+++ scripts/gh-pages-deploy.js
@@ -9,6 +9,11 @@ const fs = require('fs');
     await execa('npm', ['run', 'build']);
     // Understand if it's dist or build folder
     const folderName = fs.existsSync('dist') ? 'dist' : 'build';
+    /* * * WIP * * * */
+    await execa('fs', [
+      'fs.copyFile("./dist/index.html", "./dist/404.html")',
+    ]);
+    /* * * * * * * * */
     await execa('git', ['--work-tree', folderName, 'add', '--all']);
     await execa('git', ['--work-tree', folderName, 'commit', '-m', 'gh-pages']);
     console.log('Pushing to gh-pages...');
@@ -16,18 +21,6 @@ const fs = require('fs');
     await execa('rm', ['-r', folderName]);
     await execa('git', ['checkout', '-f', 'master']);
     await execa('git', ['branch', '-D', 'gh-pages']);
-    /* * * WIP * * * */
-    await execa('fs', [
-      'fs.copyFile("index.html", "404.html")',
-    ]);
-    // await execa('find', ['./', '-type', 'f',
-    //   '-name', '"index.html"', '|', 'while', 'read', 'fname;', 'do',
-    //   'dirname=`dirname "$fname"`', 'foldername=`basename "$dirname"`',
-    //   'filename=`basename $fname`', 'newname=`404.html`',
-    //   // eslint-disable-next-line no-template-curly-in-string
-    //   'cp "${dirname}/$filename" "./404.html"', 'done',
-    // ]);
-    /* * * * * * * * */
     console.log('Successfully deployed');
   } catch (e) {
     console.log(e.message);
